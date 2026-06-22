c929a36d0797d26b4c9363fd97402a2f3f4c2a99
Bekoo023 <162413@novacollege.nl>
2026-06-23 01:59:55 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: "C:/xampp/htdocs/BS/sales-daily",
+  outputFileTracingRoot: __dirname,
   outputFileTracingIncludes: {
     "/**": ["./app/_og/*.ttf"],
   },
